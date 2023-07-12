8a5e16a8223d29d84055c8f735120034d26c90ee
TilakTS <shakyatilak45@gmail.com>
2023-07-12 15:04:35 +0530

    Initial Commit

---
+++ DriverKitUi/DriverKitUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  DriverKitUiApp.swift
+//  DriverKitUi
+//
+//  Created by MACM05 on 12/07/23.
+//
+
+import SwiftUI
+
+@main
+struct DriverKitUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
